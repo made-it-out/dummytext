2c67f6d8c25e6cf580261caecfcc4ac128d4d4e5
made-it-out <mike.hallisy@gmail.com>
2022-01-24 01:27:01 -0500

    added fetch on homepage to get all categories and populate the select input for the form to generate text

--- public/index.js
+++ public/index.js
@@ -1,7 +1,23 @@
 document.addEventListener('DOMContentLoaded', init)
 
 function init(){
+    const categoriesList = document.getElementById("categories-list")
     // Needed
     // Call api to fill select input with categories
+    const resource = '/api/categories/all'
+
+    fetch(resource)
+    .then(response => response.json())
+    .then(categories => {
+        categories.forEach(category => {
+            // Create an option element for each category and add it to the select input
+            const element = document.createElement("option")
+            element.setAttribute("value", category)
+            element.textContent = category
+
+            categoriesList.appendChild(element)
+        })
+    })
+    .catch(error => console.log(error))
     // Handle form submission to create paragraphs
 }
